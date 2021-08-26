e1957e7cab899c857a81a97eeeb32bb3fa258f58
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-26 00:32:17 -0300

    Adiciona resolução do exercício Cebolinha ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cebolinhaFalando; 
+            Console.Write("Olá usuário! Por favor digite um texto: ");
+            cebolinhaFalando = Console.ReadLine()
+            .Replace("r", "l")
+            .Replace("R","L");
+
+            Console.WriteLine(cebolinhaFalando);
+        }
+    }
+}
